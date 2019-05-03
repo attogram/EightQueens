c1bd47186563113725252e18781ee597afa79ad6
Attogram Project <attogram@users.noreply.github.com>
2019-05-03 23:51:58 +0200

    EightQueens build v0.0.1-alpha

---
+++ precache-manifest.7f33256304776dce3bedd5fb14f32463.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60bc4607899491be7c487614ed89f837",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dc8c05f6fbb44c7c25fe",
+    "url": "./static/css/main.49211584.chunk.css"
+  },
+  {
+    "revision": "eb275f3f092d3143559c",
+    "url": "./static/js/2.2c4051bb.chunk.js"
+  },
+  {
+    "revision": "dc8c05f6fbb44c7c25fe",
+    "url": "./static/js/main.2bfd7e0e.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
