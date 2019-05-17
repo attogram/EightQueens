1cc08955c01c5096ccf75f6f4f106398952a0887
Attogram Project <attogram@users.noreply.github.com>
2019-05-17 17:23:44 +0200

    Eight Queens 0.4.2 - jokes

--- src/DidYouKnow.js
+++ src/DidYouKnow.js
@@ -10,7 +10,7 @@ const tidbits = [
     // License: Creative Commons Attribution-ShareAlike License
     "The eight queens puzzle is an example of the n queens problem of placing n non-attacking queens on an n×n chessboard.",
     "Chess composer Max Bezzel published the original eight queens puzzle in 1848.",
-    "Franz Nauck published the first solutions to the eight queens puzzle in 1850.",
+    "Franz Nauck published the a solutions to the eight queens puzzle in 1850.",
     "Many mathematicians, including Carl Friedrich Gauss, have worked on the 8 queens puzzle and its generalized n-queens version.",
     "In 1874, S. Gunther proposed a method using determinants to find solutions to the eight queens puzzle.",
     "In 1972, Edsger Dijkstra used the eight queens problem to illustrate the power of structured programming.",
@@ -47,18 +47,29 @@ const tidbits = [
     "The N-Queen problem shows a large class of nondeterministic problems that cannot be solved using deterministic algorithms in reasonable time.",
     "Many algorithms and methods have been used to resolve the n-Queen problem.",
     "Ahrabian et al. used the DNA Sticker algorithm to resolve the n-Queen problem.",
-    "Khan et al. used the ant colony algorithm to solvethe n-Queen problem.",
+    "Khan et al. used the ant colony algorithm to solve the n-Queen problem.",
     "Farhan et al. used a genetic algorithm to solve this problem and found all 92 possible solutions to the 8-queen problem.",
     "A depth-first search backtracking algorithm may resolve the n-Queens problem in acceptable time, but just for small values of n.",
     "The n-Queen problem can be successfully run with parallel genetic algorithms.",
     "The n-Queens problem is a traditional intractable problem that is used to discuss numerous kinds of searching problems.",
-    // following tidbits from EightQueens contributors:
+    // following tidbits modified from <https://upjoke.com/chess-jokes>
+    // License: acquiescence
+    "If you forget the rules of Chess don't worry... you're allowed to check.",
+    "So the other day in the lobby I hear two chess masters bragging about their past wins.  They were chess nuts boasting in an open foyer.",
+    "I went to lunch with a champion chess player.  It took them 8 minutes to pass me the salt.",
+    "Finally got a job making chess pieces.  I start on nights next week.",
+    "A computer beat me at chess.  But I really got the better of it in boxing.",
+    "Why shouldn't you eat a chess sandwich?  It's stale, mate.",
+    "Why did the chess player throw away his bread?  Because it was stalemate.",
+    // following tidbits from EightQueens contributors
+    // License: MIT License
     "Hint: how does a knight move?",
-    "Is it a chess puzzle, a chess problem, or a chess game?  Who cares, just play!",
+    "Is this a chess puzzle, a chess problem, or a chess game?",
     "Of the 92 possible solutions, there are 12 fundamental solutions.",
     "How to programmatically solve the 8 Queens problem is often used as a test during programmer interviews.",
     "How to programmatically solve the 8 Queens problem is often used as an assignment in computer science classes.",
     "Mathematicians love eight queens.",
+    "Help, I'm stuck inside an interactive, virtualized, modularized cloud-based chess game!",
 ];
 
 class DidYouKnow extends Component {
@@ -72,7 +83,7 @@ class DidYouKnow extends Component {
     }
 
     componentDidMount(){
-        this.timer = setInterval(this.tick, 7000); // 7 seconds
+        this.timer = setInterval(this.tick, 8000); // 8 seconds, of course
     }
 
     componentWillUnmount(){
